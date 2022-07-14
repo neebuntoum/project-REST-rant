94b77af5de614dba0ed3cd61698e6e58d7867869
Nee Buntoum <101156624+neebuntoum@users.noreply.github.com>
2022-07-13 22:12:31 -0400

    adding 404

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
-
 const express = require('express')
-
 const app = express()
 
-app.post('/', (req, res) => {
-    res.send('Hello Reveiwers')
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send(`<h1>404 page</h1>`)
+    
 })
 
 app.listen(process.env.PORT, function () {
